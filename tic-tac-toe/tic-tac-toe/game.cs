2da7c19b482a063180b635530fc987e5485f46e9
Антропов Павел <sidexrus@yandex.ru>
2018-05-21 06:09:52 +0700

    Test - mid line winner (passed)

--- tic-tac-toe/tic-tac-toe/game.cs
+++ tic-tac-toe/tic-tac-toe/game.cs
@@ -12,6 +12,7 @@ namespace tic_tac_toe
         public int checkresult(List<int> result)
         {
             if ((result[0] == result[1]) && (result[0] == result[2]) && result[0] != 0)  return 1;
+            if ((result[3] == result[4]) && (result[3] == result[5]) && result[3] != 0) return 1;
             if ((result[0] == result[3]) && (result[0] == result[6]) && result[0] != 0) return 1;
             if ((result[1] == result[4]) && (result[1] == result[7]) && result[1] != 0) return 1;
             if ((result[2] == result[5]) && (result[2] == result[8]) && result[2] != 0) return 1;
