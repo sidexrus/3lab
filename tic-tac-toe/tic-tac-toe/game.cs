ddfea8846c095b023e5a852ffd9b3e9ed7525bc1
Антропов Павел <sidexrus@yandex.ru>
2018-05-21 06:16:18 +0700

    Test - diagonal winner (passed)

--- tic-tac-toe/tic-tac-toe/game.cs
+++ tic-tac-toe/tic-tac-toe/game.cs
@@ -11,12 +11,13 @@ namespace tic_tac_toe
         public bool x_o = true;
         public int checkresult(List<int> result)
         {
-            if ((result[0] == result[1]) && (result[0] == result[2]) && result[0] != 0)  return 1;
+            if ((result[0] == result[1]) && (result[0] == result[2]) && result[0] != 0) return 1;
             if ((result[3] == result[4]) && (result[3] == result[5]) && result[3] != 0) return 1;
             if ((result[6] == result[7]) && (result[7] == result[8]) && result[6] != 0) return 1;
             if ((result[0] == result[3]) && (result[0] == result[6]) && result[0] != 0) return 1;
             if ((result[1] == result[4]) && (result[1] == result[7]) && result[1] != 0) return 1;
             if ((result[2] == result[5]) && (result[2] == result[8]) && result[2] != 0) return 1;
+            if ((result[0] == result[4]) && (result[0] == result[8]) && result[0] != 0) return 1;
             return 0;
         }
     }
