a82595465177d206d65b76f81588a4e0a3c0b067
Антропов Павел <sidexrus@yandex.ru>
2018-05-21 06:12:35 +0700

    Test - bottom line winner (passed)

--- tic-tac-toe/tic-tac-toe/game.cs
+++ tic-tac-toe/tic-tac-toe/game.cs
@@ -13,6 +13,7 @@ namespace tic_tac_toe
         {
             if ((result[0] == result[1]) && (result[0] == result[2]) && result[0] != 0)  return 1;
             if ((result[3] == result[4]) && (result[3] == result[5]) && result[3] != 0) return 1;
+            if ((result[6] == result[7]) && (result[7] == result[8]) && result[6] != 0) return 1;
             if ((result[0] == result[3]) && (result[0] == result[6]) && result[0] != 0) return 1;
             if ((result[1] == result[4]) && (result[1] == result[7]) && result[1] != 0) return 1;
             if ((result[2] == result[5]) && (result[2] == result[8]) && result[2] != 0) return 1;
